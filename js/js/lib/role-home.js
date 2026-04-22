22d9bdc4d72a4a8f41cd7ccf02cf352322e20a6f
sahil-7-dev <SAHILBIND28@GMAIL.COM>
2026-04-22 08:13:19 +0530

    Delete js/js directory

--- js/js/lib/role-home.js
+++
@@ -1,7 +0,0 @@
-// js/lib/role-home.js
-
-export function roleHome(role) {
-  if (role === "admin")   return "admin.html";
-  if (role === "company") return "dashboard-company.html";
-  return "dashboard.html";
-}
